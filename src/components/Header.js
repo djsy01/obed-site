b648c3389da594412617b374aacc7d55e37e5c75
djsy01 <dlsgh8884@naver.com>
2025-05-01 15:04:38 +0900

    해더 수정

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,7 @@ import './Header.css';
 function Header() {
   return (
     <header>
+      <img src={process.env.PUBLIC_URL + '/assets/image/LOG.JPG'} alt="OBED Worship Logo" className="logo" />
       <h1>OBED Worship</h1>
       <p>하나님을 예배하는 공동체</p>
     </header>
